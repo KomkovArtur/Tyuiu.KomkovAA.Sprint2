fc07fdee92fa4928e3f3d3f6b8c26562ee56f58b
артур комков <stenplay33@gmail.com>
2025-10-16 18:55:17 +0500

    Task5

---
+++ Tyuiu.KomkovAA.Sprint2.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KomkovAA.Sprint2.Task5.V6.Lib;
+namespace Tyuiu.KomkovAA.Sprint2.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("туз пик", ds.FindCardNameAndValue(1, 14));
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint2.Task5.V6/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KomkovAA.Sprint2.Task5.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Оператор switch                                                   *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Мастям игральных карт условно присвоены следующие порядковые номера:    *");
+        Console.WriteLine("* масти «пики» — 1, масти «трефы» — 2, масти «бубны» — 3, масти «червы»   *");
+        Console.WriteLine("* — 4, а достоинству карт: «валету» — 11, «даме» — 12, «королю» — 13,     *");
+        Console.WriteLine("* «тузу» — 14 (порядковые номера карт остальных достоинств соответствуют  *");
+        Console.WriteLine("* их названиям: «шестерка», «девятка» и т. п.). По заданным номеру масти  *");
+        Console.WriteLine("* m (1 <= m <= 4) и номеру достоинства карты k (6 <= k <= 14) определить  *");
+        Console.WriteLine("* полное название (масть и достоинство) соответствующей карты в виде      *");
+        Console.WriteLine("* «Дама пик», Шестерка бубен» и т. п.                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        int a, b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        var result = ds.FindCardNameAndValue(a, b);
+        Console.WriteLine(result);
+    }
+}

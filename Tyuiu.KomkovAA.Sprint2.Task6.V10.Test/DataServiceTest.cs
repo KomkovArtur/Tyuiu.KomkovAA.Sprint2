dfe0a8ff10631723e0f420ca509662a6183b0155
артур комков <stenplay33@gmail.com>
2025-10-16 19:50:11 +0500

    Task6

---
+++ Tyuiu.KomkovAA.Sprint2.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KomkovAA.Sprint2.Task6.V10.Lib;
+namespace Tyuiu.KomkovAA.Sprint2.Task6.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("15.10.2025", ds.FindDateOfPreviousDay(2025, 10, 16));
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KomkovAA.Sprint2.Task6.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Получение результата из switch                                    *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дата некоторого дня характеризуется тремя натуральными числами: g (год),*");
+        Console.WriteLine("* m (порядковый номер месяца) и n (число). По заданным g, n и m           *");
+        Console.WriteLine("* определить дату предыдущего дня. Заданный год не является високосным.   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        Console.WriteLine("Введите номер дня: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер месяца: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер года: ");
+        int g = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              ");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("Дата предыдущего дня: " + ds.FindDateOfPreviousDay(g, m, n));
+
+
+    }
+}

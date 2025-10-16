b831fef4dd7a2759ffaae72250ff8b9a7256173e
артур комков <stenplay33@gmail.com>
2025-10-16 18:10:26 +0500

    Task4

---
+++ Tyuiu.KomkovAA.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KomkovAA.Sprint2.Task4.V15.Lib;
+namespace Tyuiu.KomkovAA.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double z = ds.Calculate(x, y);
+            double wait = 3.5;
+            Assert.AreEqual(wait, z);
+        }
+    }
+}
